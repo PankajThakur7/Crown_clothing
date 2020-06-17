00682ba47086648e4dba97133aed296bd878137c
Pankaj Thakur <Pankaj.thakurdgp@gmail.com>
2020-06-18 00:30:21 +0530

    Added item collection and collection preview components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {Switch,Route} from 'react-router-dom'
 import './App.css';
 import Homepage from './components/page/homepage/homepage.component';
+import ShopPage from './components/shop/shop.compononet';
 
 function App() {
   return (
     <div className="App">
   <Switch>
   <Route exact  path="/" component={Homepage}></Route>
+  <Route exact path="/shop" component={ShopPage}></Route>
   </Switch>
     </div>
   );
